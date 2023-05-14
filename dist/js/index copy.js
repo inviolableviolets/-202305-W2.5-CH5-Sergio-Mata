7a8b925b8a6c5e39a5e46bed91aefc9f2d21ad4d
Sergio Mata <sergiomataherrero@gmail.com>
2023-05-14 21:33:17 +0200

    Delete the duplicated files and relocate index.js to the js folder

--- dist/js/index copy.js
+++
@@ -1,8 +0,0 @@
-import { createBoard, printBoard } from "./board.js";
-let generation = 0;
-setInterval(() => {
-    const board = createBoard(10, 20);
-    console.log(printBoard(board));
-    generation++;
-    console.log(`Generation: ${generation}`);
-}, 300);
