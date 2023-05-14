67f31c2e621fc4486a90e941abb47625ed95ba8a
Sergio Mata <sergiomataherrero@gmail.com>
2023-05-14 14:09:56 +0200

    Add neighbour counter

--- src/neighbours.ts
+++ src/neighbours.ts
@@ -2,7 +2,11 @@ import { createBoard } from "./board.js";
 
 const board = createBoard(10, 20);
 
-export const checkNeighbourPossiblePositions = (column = 0, row = 0) => {
+export const checkNeighbourPossiblePositions = (
+  column = 0,
+  row = 0,
+  cell: number[][]
+) => {
   const possiblePositions = [
     [column - 1, row - 1],
     [column - 1, row],
@@ -16,3 +20,26 @@ export const checkNeighbourPossiblePositions = (column = 0, row = 0) => {
 
   return possiblePositions;
 };
+
+const cellExample = [
+  [0, 0, 0],
+  [1, 1, 1],
+  [1, 1, 0],
+];
+
+const countNeighbours = (cell: number[][]) => {
+  let neighboursCount = 0;
+
+  for (let i = 0; i < cell.length; i++) {
+    for (let j = 0; j < cell[i].length; j++) {
+      if (cell[i][j] === 1) {
+        neighboursCount++;
+      }
+    }
+  }
+  if (cell[1][1] === 1 && neighboursCount > 0) neighboursCount--;
+
+  return neighboursCount;
+};
+
+console.log(countNeighbours(cellExample));
